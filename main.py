bdf571949f736583fe40f259e7e1f818c06a2ea9
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-19 20:36:56 -0300

    Feat: Displaying selected image

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from tkinter import Tk, StringVar, Button, Entry
+from tkinter import Tk, StringVar, Button, Entry, Label
+from tkinter import filedialog
+from PIL import Image, ImageTk
 
 
 root = Tk()
@@ -8,7 +10,20 @@ root.title("Watermark")
 WATERMARK_TEXT = StringVar()
 WATERMARK_TEXT.set("")
 
-open_button = Button(root, text="Select image")
+
+def select_image():
+    file_path = filedialog.askopenfilename(filetypes=[("Image", "*.png;*.jpg;*.jpeg;*.gif")])
+    if file_path:
+        image_path = file_path
+        original_image = Image.open(image_path)
+        tk_image = ImageTk.PhotoImage(original_image)
+        
+        image_label = Label(root, image=tk_image)
+        image_label.image = tk_image
+        image_label.pack()
+
+
+open_button = Button(root, text="Select image", command=select_image)
 open_button.pack(pady=10)
 
 watermark_entry = Entry(root, textvariable=WATERMARK_TEXT, width=30)
